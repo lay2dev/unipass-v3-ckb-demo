bbe7cf1579c947e42185980f08f3478bac76a689
ItVen <751733381@qq.com>
2021-11-01 21:49:03 +0800

    demo init

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="page-index">
+    <h1>UniPass L3 Demo</h1>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      helloAven: 'Aven',
+    }
+  },
+  created() {
+    console.log('🌊', 'evan')
+  },
+})
+</script>
+<style lang="stylus">
+#page-index {
+}
+</style>
