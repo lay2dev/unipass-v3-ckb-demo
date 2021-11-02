048518f37060349a3e8a397c944782ae1097cdc3
ItVen <751733381@qq.com>
2021-11-02 20:16:20 +0800

    update demo event

---
+++ assets/js/up-types.ts
@@ -0,0 +1,53 @@
+/* eslint-disable no-useless-constructor */
+export class UPAccount {
+  constructor(public username: string, public email?: string) {}
+}
+export type keyType = 'Secp256k1Pubkey' | 'Secp256r1Pubkey' | 'RsaPubkey'
+
+export class UPAuthResponse {
+  constructor(
+    public keyType: keyType,
+    public pubkey: string,
+    public sig: string,
+  ) {}
+}
+
+// ---------- AUTHORIZE ----------
+type AUTH_TYPE = 'PLAIN_MSG' | 'CKB_TX' | 'EVM_TX' | 'FLOW_TX'
+type AUTH_HASH = 'sha256' | 'sha3' | 'blake2b'
+
+export class UPAuthMessage {
+  constructor(
+    public readonly type: AUTH_TYPE,
+    public readonly username: string,
+    public readonly payload: string,
+    public readonly hash: AUTH_HASH = 'sha256',
+  ) {}
+}
+
+type RESPONSE_TYPE = 'APPROVE' | 'DECLINE'
+export class UPResponse {
+  constructor(
+    public readonly type: RESPONSE_TYPE,
+    public readonly data: UPAccount | UPAuthResponse | string,
+  ) {}
+}
+export type UPMessageType =
+  | 'UP_READY'
+  | 'UP_RESPONSE'
+  | 'UP_CLOSE'
+  | 'UP_AUTH'
+  | 'UP_LOGIN'
+  | 'UP_ERROR'
+
+// ------------ CONNECT ------------
+export class UPMessage {
+  constructor(
+    public type: UPMessageType,
+    public payload?: string, // public resolve?: any, // public reject?: any
+  ) {}
+}
+
+export type UPConnectOptions = {
+  email: boolean
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,87 @@
 <template>
-  <div id="page-index">
-    <h1>UniPass L3 Demo</h1>
+  <div>
+    <b>UniPass Demo</b>
+    <br />
+    <div>
+      <br />
+      <h3>{{ username }}</h3>
+      <br />
+    </div>
+    <div v-if="username">
+      <el-button type="info" @click="logout">logout</el-button>
+      <!-- <el-button type="primary">home</el-button> -->
+    </div>
+    <div v-else>
+      <el-button type="primary" @click="connect">login</el-button>
+    </div>
+    <br />
+    <div>
+      <br />
+      <el-input v-model="message" type="textarea" :rows="2"> </el-input>
+
+      <br />
+      <div>
+        <el-button type="primary" @click="authorize">authorize</el-button>
+      </div>
+      <br />
+      <el-input v-if="signData" v-model="signData" type="textarea" :rows="8">
+      </el-input>
+    </div>
   </div>
 </template>
+
 <script lang="ts">
 import Vue from 'vue'
+import UP from 'up-core'
+import { UPAuthMessage } from '~/assets/js/up-types'
+
 export default Vue.extend({
   data() {
     return {
-      helloAven: 'Aven',
+      username: '',
+      message: 'TO BE SIGNED MESSAGE abc',
+      signData: '',
     }
   },
-  created() {
-    console.log('🌊', 'evan')
+  mounted() {
+    UP.config(
+      'http://localhost:3000/login',
+      'http://localhost:3000/login',
+      'http://localhost:3000/sign',
+    )
+  },
+  methods: {
+    async connect() {
+      console.log('connect clicked')
+      try {
+        const account = await UP.connect()
+        this.username = account.username
+        console.log('account', account)
+      } catch (err) {
+        this.$message.error(err as string)
+        console.log('connect err', err)
+      }
+    },
+    logout() {
+      console.log('connect clicked')
+      UP.disconnect()
+      this.username = ''
+    },
+    async authorize() {
+      console.log('authorize clicked')
+      this.signData = ''
+      console.log({ username: this.username, message: this.message })
+      try {
+        const resp = await UP.authorize(
+          new UPAuthMessage('PLAIN_MSG', this.username, this.message),
+        )
+        console.log('resp', resp)
+        this.signData = JSON.stringify(resp)
+      } catch (err) {
+        this.$message.error(err as string)
+        console.log('auth err', err)
+      }
+    },
   },
 })
 </script>
-<style lang="stylus">
-#page-index {
-}
-</style>
